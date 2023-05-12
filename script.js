cdd2cfc6bb929a72a8a6fa48a4451fdedf257eba
Greg Howes <gihowes@gmail.com>
2023-05-12 11:06:57 +0900

    Basic interface and logic working Game doesn't yet finish when one player reaches 5 points

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let humanScore = 0;
+let computerScore = 0;
+
+//event listeners
+const buttons = document.querySelectorAll("button");
+  buttons.forEach(button => button.addEventListener("click", game));
+
+const resultText = document.querySelector(".result");
+const paraHumanScore = document.querySelector(".human-score");
+const paraComputerScore = document.querySelector(".computer-score");
+
+
 //Return a random choice of either Rock, Scissors or Paper
 function getComputerChoice() {
     const randomInt = Math.floor(Math.random() * 3);
@@ -13,7 +25,7 @@ function getComputerChoice() {
 //Play single round and return a string to show the winner
 function playRound(playerSelection, computerSelection) {
     const playerChoice = playerSelection.toUpperCase();
-    console.log(computerSelection);
+    //console.log(computerSelection);
     if (playerChoice == computerSelection) {
         //check for draw first
         //I think this reduces the checking we have to do later
@@ -35,12 +47,13 @@ function playRound(playerSelection, computerSelection) {
 }
 
 //play five rounds, keeping track of score and outputting the winner's name
+/*
 function game() {
     let computerWins = 0;
     let playerWins = 0;
     for (let gameCount = 0; gameCount < 5; gameCount++) {
         let result = playRound (prompt("Enter Rock, Scissors or Paper"), getComputerChoice());
-        console.log(result);
+        //console.log(result);
         if (result.includes("Human")) {
             playerWins++;
         } else if (result.includes("Computer")) {
@@ -54,5 +67,35 @@ function game() {
     } else {
         return `Computer wins with ${computerWins} vs ${playerWins}`;
     }
+}
+*/
 
+
+function game(e) {
+  //console.log(this.id);
+  console.log(resultText);
+  resultText.innerText = playRound(this.id, getComputerChoice());
+  updateScores();
+}
+
+function updateScores() {
+    console.log(resultText.innerText.includes("Human"));
+    
+    if (resultText.innerText.includes("Human")) {
+        //incremement player score
+        humanScore++;
+        paraHumanScore.innerText = "Your score: " + humanScore;
+        console.log(paraHumanScore);
+
+    } else if (resultText.innerText.includes("Computer")) {
+        //incremement computer score
+        computerScore++;
+        paraComputerScore.innerText = "Computer score: " + computerScore;
+    }
+
+    if (computerScore == 5 || humanScore == 5) {
+        resultText.innerText += "Game over, well done!!"
+        humanScore = 0;
+        computerScore = 0;
+    }
 }
